ca3a7c839472fc83099c112fe555592a0b98a96c
SilentPing <your-email@example.com>
2025-09-11 12:27:39 +0800

    Removed hardcoded db credentials

--- registration.php
+++ registration.php
@@ -358,9 +358,6 @@
 
                           require_once __DIR__ . '/db.php';
 
-                          // Create a connection
-                          $conn = new mysqli($servername, $username, $password, $dbname);
-
                           // Check the connection
                           if ($conn->connect_error) {
   				                    die("Connection failed: " . $conn->connect_error);
