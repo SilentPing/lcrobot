fa2fd33ee86b9962ea578576dcb4dc96fbb28ab7
SilentPing <your-email@example.com>
2025-09-13 14:11:22 +0800

    Fix footer

--- includes/footer.php
+++ includes/footer.php
@@ -4,19 +4,11 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; BMCROP</span>
+            <span>Copyright &copy; BMCROP 2025</span>
           </div>
         </div>
       </footer>
       <!-- End of Footer -->
-      </div>
-        <!-- /.container-fluid -->
-
-      </div>
-      <!-- End of Main Content -->
-    </div>
-    <!-- End of Content Wrapper -->
-
     
 </body>
 
